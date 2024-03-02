8432f9ecffc25d1ec7d85ec2c2efff8a7ea1ac9c
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2024-03-02 13:43:29 -0500

    Separate prompts and introduce connections between functions

--- server.js
+++ server.js
@@ -38,49 +38,98 @@ connection.connect((err) => {
           'Update an employee role',
           'Exit',
         ],
-      })
+      })}
+
+//view table items
+function viewDepartments() {
+    connection.query('SELECT * FROM department', (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      startApp();
+    });
+  }
+  
+  function viewRoles() {
+    connection.query(
+      'SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id',
+      (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+      }
+    );
+  }
+  
+  function viewEmployees() {
+    connection.query(
+      `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+      FROM employee
+      LEFT JOIN role ON employee.role_id = role.id
+      LEFT JOIN department ON role.department_id = department.id
+      LEFT JOIN employee manager ON employee.manager_id = manager.id`,
+      (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+      }
+    );
+  }
+
+  function addRole() {
+    connection.query('SELECT * FROM department', (err, departments) => {
+      if (err) throw err;
+  
       inquirer
-      .prompt([
-        {
-          name: 'title',
-          type: 'input',
-          message: 'Enter the title of the role:',
-        },
-        {
-          name: 'salary',
-          type: 'input',
-          message: 'Enter the salary for this role:',
-        },
-        {
-          name: 'department',
-          type: 'list',
-          message: 'Select the department for this role:',
-          choices: departments.map((department) => department.name),
-        },
-      ])    
-      inquirer
-          .prompt([
-            {
-              name: 'first_name',
-              type: 'input',
-              message: 'Enter the first name of the employee:',
-            },
-            {
-              name: 'last_name',
-              type: 'input',
-              message: 'Enter the last name of the employee:',
-            },
-            {
-              name: 'role',
-              type: 'list',
-              message: 'Select the role for this employee:',
-              choices: roles.map((role) => role.title),
-            },
-            {
-              name: 'manager',
-              type: 'list',
-              message: 'Select the manager for this employee:',
-              choices: managers.map((manager) => `${manager.first_name} ${manager.last_name}`),
-            },
-          ])
-    }
+        .prompt([
+          {
+            name: 'title',
+            type: 'input',
+            message: 'Enter the title of the role:',
+          },
+          {
+            name: 'salary',
+            type: 'input',
+            message: 'Enter the salary for this role:',
+          },
+          {
+            name: 'department',
+            type: 'list',
+            message: 'Select the department for this role:',
+            choices: departments.map((department) => department.name),
+          },
+        ])}
+
+        function addEmployee() {
+            connection.query('SELECT * FROM role', (err, roles) => {
+              if (err) throw err;
+          
+              connection.query(
+                'SELECT * FROM employee WHERE manager_id IS NULL',
+                (err, managers) => {
+                  if (err) throw err;
+          
+                  inquirer
+                    .prompt([
+                      {
+                        name: 'first_name',
+                        type: 'input',
+                        message: 'Enter the first name of the employee:',
+                      },
+                      {
+                        name: 'last_name',
+                        type: 'input',
+                        message: 'Enter the last name of the employee:',
+                      },
+                      {
+                        name: 'role',
+                        type: 'list',
+                        message: 'Select the role for this employee:',
+                        choices: roles.map((role) => role.title),
+                      },
+                      {
+                        name: 'manager',
+                        type: 'list',
+                        message: 'Select the manager for this employee:',
+                        choices: managers.map((manager) => `${manager.first_name} ${manager.last_name}`),
+                      },
+                    ])}
