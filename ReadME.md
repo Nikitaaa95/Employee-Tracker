cfad53d508752c5091028b35e0e924ebbfbfe631
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2024-03-02 16:28:07 -0500

    done debugging

--- ReadME.md
+++ ReadME.md
@@ -2,3 +2,6 @@ Sample Data Provided by Assignment
 
 Resolving inquirer error:
 https://stackoverflow.com/questions/61126142/import-inquirer-as-module-in-node-13
+
+Assistance with connection issues
+SALVARADO from the ASK BCS Learning Assistant
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,29 @@
 USE company_db;
 
---departments
+/* departments */
 INSERT INTO department (name)
 VALUES ('Sales'),
        ('Engineering'),
        ('Finance'),
        ('Legal');
 
---roles
+/* roles */
 INSERT INTO role (title, salary, department_id) 
 VALUES ('Salesperson', 80000, 1),
        ('Lead Engineer', 150000, 2),
        ('Software Engineer', 120000, 2),
        ('Account Manager', 160000, 3),
        ('Accountant', 125000, 3),
-       ('Legal Team Lead', 250000, 4),       
+       ('Legal Team Lead', 250000, 4),
        ('Lawyer', 190000, 4);
 
---employees
+/* employees */
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES ('John', 'Doe', 1, NULL),
        ('Mike', 'Chan', 1, 1),
-       ('Ashley', 'Rodriguez', 2, null),
+       ('Ashley', 'Rodriguez', 2, NULL),
        ('Kevin', 'Tupik', 3, 3),
-       ('Kunal', 'Singh', 4, null),
+       ('Kunal', 'Singh', 4, NULL),
        ('Malia', 'Brown', 5, 5),
-       ('Sarah', 'Lourd', 6, null),
+       ('Sarah', 'Lourd', 6, NULL),
        ('Tom', 'Allen', 7, 7);
