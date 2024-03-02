dacf0a3acc62b6af6851348f4ec92e320a2e590b
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2024-03-02 13:10:47 -0500

    Adding prompts

--- server.js
+++ server.js
@@ -20,3 +20,67 @@ connection.connect((err) => {
     console.log('Connected to the database.');
     startApp();
   });
+
+  //prompts
+  function startApp() {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit',
+        ],
+      })
+      inquirer
+      .prompt([
+        {
+          name: 'title',
+          type: 'input',
+          message: 'Enter the title of the role:',
+        },
+        {
+          name: 'salary',
+          type: 'input',
+          message: 'Enter the salary for this role:',
+        },
+        {
+          name: 'department',
+          type: 'list',
+          message: 'Select the department for this role:',
+          choices: departments.map((department) => department.name),
+        },
+      ])    
+      inquirer
+          .prompt([
+            {
+              name: 'first_name',
+              type: 'input',
+              message: 'Enter the first name of the employee:',
+            },
+            {
+              name: 'last_name',
+              type: 'input',
+              message: 'Enter the last name of the employee:',
+            },
+            {
+              name: 'role',
+              type: 'list',
+              message: 'Select the role for this employee:',
+              choices: roles.map((role) => role.title),
+            },
+            {
+              name: 'manager',
+              type: 'list',
+              message: 'Select the manager for this employee:',
+              choices: managers.map((manager) => `${manager.first_name} ${manager.last_name}`),
+            },
+          ])
+    }
